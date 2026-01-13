38d4eab8524dafb2ca2a10d6d0ad5b183cc0b8ef
vctr <victorhenzzowp@gmail.com>
2026-01-12 21:46:49 -0300

    first commit

---
+++ src/test/java/com/wykmmam/rest_spring_java/SetupTests.java
@@ -0,0 +1,13 @@
+package com.wykmmam.rest_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
