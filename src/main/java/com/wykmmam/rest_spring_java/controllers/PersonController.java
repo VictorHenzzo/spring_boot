98fd60cb283d40ff294667ca93b90eaa5ee4e489
vctr <victorhenzzowp@gmail.com>
2026-01-14 07:23:36 -0300

    remove math and greeting code

---
+++ src/main/java/com/wykmmam/rest_spring_java/controllers/PersonController.java
@@ -0,0 +1,8 @@
+package com.wykmmam.rest_spring_java.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+    
+}
--- src/main/java/com/wykmmam/rest_spring_java/model/Greeting.java
+++
@@ -1,5 +0,0 @@
-package com.wykmmam.rest_spring_java.model;
-
-public record Greeting(long id, String content) {
-
-}
