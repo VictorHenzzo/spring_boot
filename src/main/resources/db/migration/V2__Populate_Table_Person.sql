d226f3a506477c771fbf2c5013450166438fb19f
vctr <victorhenzzowp@gmail.com>
2026-01-26 17:14:37 -0300

    feat: add migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.person (address, first_name, last_name)
+VALUES ('123 Main St, New York, NY', 'John', 'Doe'),
+       ('456 Elm St, Los Angeles, CA', 'Jane', 'Smith'),
+       ('789 Oak Ave, Chicago, IL', 'Michael', 'Johnson'),
+       ('321 Pine Rd, Houston, TX', 'Emily', 'Brown'),
+       ('654 Maple Blvd, Phoenix, AZ', 'Daniel', 'Taylor');
